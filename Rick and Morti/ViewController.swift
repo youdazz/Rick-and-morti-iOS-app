29f1593a3cde07a88af9a12fd23dda440e6dfe7d
Youchen Zhou <yzyouchen@gmail.com>
2023-02-04 12:45:57 +0100

    Initial Commit

---
+++ Rick and Morti/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick and Morti
+//
+//  Created by Youchen Zhou on 4/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
